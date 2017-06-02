61a69158661944bf6f3c46511e9585f29cab3567
ruandong <ssyz@outlook.com>
2017-06-02 21:06:31 +0800

    加入压缩包

---
+++ khancode/myCode.js
@@ -0,0 +1,25 @@
+
+var sketchProc=function(processingInstance){
+with (processingInstance){
+size(400, 400); 
+frameRate(30);
+background(255,255,255);
+fill(255,0,0);
+
+//请不要改动上面的代码！
+// --------------开始写你的代码 ----------- 
+
+rect(150,150,100,100);
+
+
+
+
+
+var draw=function(){
+	
+};
+
+//请不要改动下面的代码！		
+ 
+}
+};
